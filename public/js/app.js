80da7b253074f61923472103c29ce88f4f73146a
vincaf <vincenzocafassog@gmail.com>
2022-09-26 16:14:58 +0200

    feat(index): add optionals in the table

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Genusia\Desktop\visual studio\laravel\laravel-auto-usate\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Genusia\Desktop\visual studio\laravel\laravel-auto-usate\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\vince\Desktop\Boolean\Esercizi\laravel-auto-usate\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\vince\Desktop\Boolean\Esercizi\laravel-auto-usate\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
